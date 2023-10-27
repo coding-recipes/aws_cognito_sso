913ee66e24eee7fbad5774cb18755710bc076934
Peter Maczo <peter.maczo@gmail.com>
2023-10-27 14:03:47 +0200

    ui-1: deployment

--- ui-1-react/deploy.sh
+++ ui-1-react/deploy.sh
@@ -1,29 +1,27 @@
-if [ "$1" != "be1" ] && [ "$1" != "be2" ]; then
-  echo "----- ERR: \$1 should be either 'be1' or 'be2' -----"
+if [ "$1" != "1" ] && [ "$1" != "2" ]; then
+  echo "----- ERR: \$1 should be either '1' or '2' -----"
   exit 1
 fi
 
-source .env.$1
+ENV=be$1
+
+source .env.$ENV
 
 echo "----- S3 bucket: $S3_BUCKET"
 echo "----- S3 folder: $S3_FOLDER"
 echo "----- CLF_DIST: $CLF_DIST"
 echo "----- PROFILE: $PROFILE"
 
-# if [ ! -d "./dist" ]; then
-  # echo "ERR: dist directory not found"
-  # yarn build
-# fi
-
 rm -rf dist
-yarn build \-\-mode $1
+yarn build --mode $ENV
+
 cd dist
 
 echo "----- Deploying to S3 bucket: $S3_BUCKET"
 aws s3 sync . s3://$S3_BUCKET/$S3_FOLDER/ --profile $PROFILE
 
 echo "----- Setting cache-control to 0"
-aws s3 cp index.html s3://$S3_BUCKET/index.html --cache-control max-age=0 --profile $PROFILE
+aws s3 cp index.html s3://$S3_BUCKET/$S3_FOLDER/index.html --cache-control max-age=0 --profile $PROFILE
 
 echo "----- Invalidating CloudFront cache"
-aws cloudfront create-invalidation --distribution-id $CLF_DIST --paths '/index.html' --profile $PROFILE
+aws cloudfront create-invalidation --distribution-id $CLF_DIST --paths /$S3_FOLDER/index.html --profile $PROFILE
